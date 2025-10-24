b1d6fcdf521b4bf95ed59d63bcc35dbc4b08c402
你的GitHub用户名 <GitHub>
2025-10-24 10:12:53 +0800

    优化-16

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -322,9 +322,24 @@ const playVideo = () => {
     videoPlaying.value = true
 }
 
-// 组件挂载
+// 广告联盟
+const adProvider = () => {
+    const script = document.createElement('script')
+    script.src = 'https://a.magsrv.com/ad-provider.js'
+    script.async = true
+    script.type = 'application/javascript'
+    document.head.appendChild(script)
+
+    script.onload = () => {
+        if (window.AdProvider) {
+            window.AdProvider.push({ "serve": {} })
+        }
+    }
+}
+
+// 优化的组件挂载 - 避免强制重排
 onMounted(() => {
-    console.log('HomeView mounted')
+    adProvider()
 })
 
 // 组件卸载时清理
